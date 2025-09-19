fd0c0cdda3afda6c15efeb700165245fe586ce54
drakedrac <sidhuyash2006@gmail.com>
2025-09-19 16:50:09 +0530

    added getMovie logic

--- main.go
+++ main.go
@@ -3,16 +3,40 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+var apiKey string
 
 func webServerTest(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got req")
 	io.WriteString(w, "yoyo")
 }
 
-func main() {
-	http.HandleFunc("/", webServerTest)
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Query().Get("title")
+	fmt.Println(apiKey)
+	resp, e := http.Get("http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + title)
+	if e == nil {
+	}
+	defer resp.Body.Close()
 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+	}
+	fmt.Println("Error:", e)
+	io.WriteString(w, string(body))
+}
+
+func main() {
+	godotenv.Load()
+	apiKey = os.Getenv("OMDB_KEY")
+	http.HandleFunc("/", webServerTest)
+	http.HandleFunc("/api/movie", getMovie)
+	fmt.Println("starting server at http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
 }
